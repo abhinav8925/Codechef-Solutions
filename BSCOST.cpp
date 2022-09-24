abe2967f6632c5603f7b301193bd38bf902fdf0a
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2022-09-24 14:40:14 +0530

    Create BSCOST.cpp
    commit

---
+++ BSCOST.cpp
@@ -0,0 +1,54 @@
+// Date - 24th August 2022
+// Code by - Abhinav Anand
+
+// Problem link -> https://www.codechef.com/problems/BSCOST
+
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void solve()
+{
+    int n,x,y;
+    cin>>n>>x>>y;
+    string s;
+    cin>>s;
+    int cntz=0;
+    int cnto=0;
+    for(int i=0;i<n;i++)
+        {
+            if(s[i]=='0')
+                cntz++;
+            else if(s[i]=='1')
+                cnto++;
+        }
+    sort(s.begin(),s.end());
+    if(x>y && cntz>=1 && cnto>=1)
+    {
+        cout << y << endl;
+    }
+    else if(y>x && cntz>=1 && cnto>=1)
+    {
+        cout << x << endl;
+    }
+    else if(x==y && cntz>=1 && cnto>=1)
+    {
+        cout << y << endl;
+    }
+    else if(cntz<1 || cnto<1)
+    {
+        cout << 0 << endl;
+    }
+}
+
+int main() {
+	
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+	    solve();
+	}
+	return 0;
+}
