766539de5b788dde6d6150249d1c8861ffa5a32a
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-01-18 04:45:22 +0530

    Create fit_in_datatype.cpp

---
+++ fit_in_datatype.cpp
@@ -0,0 +1,46 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date - 18th January 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/DATATYPE
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    cout << b%(a+1) << endl; 
+}
+
+int main() {
+	
+	ll t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    solve();
+	}
+	return 0;
+}
