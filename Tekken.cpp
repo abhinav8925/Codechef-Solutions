62bbc28d73623f2bb17ce3ba5eafff294ceb3f38
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-02-27 23:53:14 +0530

    Create Tekken.cpp

---
+++ Tekken.cpp
@@ -0,0 +1,50 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date - 27th February 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/TEKKEN
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if((a-abs(b-c))>0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    
+}
+
+int main() {
+	
+	ll t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    solve();
+	}
+	return 0;
+}
