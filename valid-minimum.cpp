65709223c3972ab4dff0c30b3e90562ecf66ccea
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-03-16 02:47:15 +0530

    Update valid-minimum.cpp

--- valid-minimum.cpp
+++ valid-minimum.cpp
@@ -1,3 +1,83 @@
+// 1st solution
+
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date - 16th March 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/VALIDMIN
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(min(a,b)==min(b,c) && min(a,b)==min(a,c))
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    
+}
+
+int main() {
+	
+	ll t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    solve();
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2nd solution
+
 /*
   ***** !!!!! JAI SHREE KRISHNA !!!!! *****
   
