bc8af881988eeefc809431a22d4f7640ef865bfa
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-05-04 21:56:44 +0530

    Update and rename vbhnjk.cpp to Largest and Second Largest.cpp

---
+++ Largest and Second Largest.cpp
@@ -0,0 +1,90 @@
+
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date - 4th May 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/LARGESECOND
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+vector<int> arr(N,true);
+vector<int> prime_num;
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    int x=0;
+    for(int i=n-1;i>=0;)
+    {
+        if(a[i]==a[i-1])    
+            i--;
+        else
+           {
+               x=a[--i];
+               break;
+           }
+    }
+    
+    cout << a[n-1]+x << endl;
+    
+    
+}
+
+int main() {
+	
+	ll t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    solve();
+	}
+	return 0;
+}
