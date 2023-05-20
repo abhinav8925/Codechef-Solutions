156d9ef5bf2c46234e813dfc7a6807eae363ae40
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-05-21 04:26:42 +0530

    Update and rename jk k.cpp to How Many Maximums.cpp

---
+++ How Many Maximums.cpp
@@ -0,0 +1,112 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date - 21st May 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/HOWMANYMAX?tab=statement
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int cnt=0;
+    for(int i=0;i<n-1;i++)
+    {
+        if(s[i]=='1')
+        {
+            cnt++;
+            while(s[i]=='1')
+                i++;
+        }
+    }
+    if(s[n-2]=='0')
+        cout << cnt+1 << endl;
+    else
+        cout << cnt << endl;
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
