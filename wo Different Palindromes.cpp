40ee58a1ed800fe0a56e2ad15b7872a9e5635aad
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-05-28 12:09:21 +0530

    Create wo Different Palindromes.cpp

---
+++ wo Different Palindromes.cpp
@@ -0,0 +1,108 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date - 28th May 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/TWODIFFPALINj
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    ll a,b;
+    cin>>a>>b;
+    ll sum=a+b;
+    if(sum%2!=0 && min(a,b)!=1)
+        cout << "Yes" << endl;
+    else if(sum%2!=0 && min(a,b)==1)
+        cout << "No" << endl;
+    else if(sum%2==0)
+    {
+        if(a%2==0 && b%2==0)
+            cout << "Yes" << endl;
+        else if(a%2!=0 && b%2!=0)
+            cout << "No" << endl;
+    }
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
