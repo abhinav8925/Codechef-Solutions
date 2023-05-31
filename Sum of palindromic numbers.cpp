b531c29af7f1c16cf574e2d24fc9424e483f9401
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-05-31 17:46:23 +0530

    Create Sum of palindromic numbers.cpp

---
+++ Sum of palindromic numbers.cpp
@@ -0,0 +1,121 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date - 31st May 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/SPALNUM
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+bool isPalindrome(int x) {
+        
+        long long  ans=0;
+        if(x<0 || (x!=0 && x%10==0))
+            return false;
+        while(x>ans)
+        {
+            ans=ans*10+(x%10);
+            x/=10;
+        }
+        if(ans==x || x==ans/10)
+                return true;
+        else
+                return false;
+}
+
+void solve()
+{
+    ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    int l,r;
+    cin>>l>>r;
+    int cnt=0;
+    for(int i=l;i<=r;i++){
+        if(isPalindrome(i))
+            {
+                cnt+=i;
+            }
+    }
+    cout << cnt << endl;
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
