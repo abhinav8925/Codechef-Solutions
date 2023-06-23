dfacd4240e214c84ff21e191fe3df5eca0c5c56d
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-06-23 13:03:34 +0530

    Create Swapping Chefs Way.cpp

---
+++ Swapping Chefs Way.cpp
@@ -0,0 +1,118 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 23rd June 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/SWAPCW
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    // ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    
+    
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    
+    for(int i=0,j=n-1;i<=j;i++,j--){
+        if(s[i]>s[j]){
+            swap(s[i],s[j]);
+        }
+    }
+    
+    for(int i=0;i<n-1;i++){
+        if(s[i]>s[i+1]){
+            cout << "NO" << endl;
+            return;
+        }
+    }
+
+    cout << "YES" << endl;
+    
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
