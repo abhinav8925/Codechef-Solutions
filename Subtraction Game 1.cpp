a6e3bf306669521b9246ec55670b3a8e69640055
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-08-06 20:35:34 +0530

    Create Subtraction Game 1.cpp

---
+++ Subtraction Game 1.cpp
@@ -0,0 +1,113 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 6th August 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/AMSGAME1
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    // ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    
+    ll n;
+    cin>>n;
+    ll a[n];
+    for(ll i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+    
+    ll gv=a[0];
+    
+    for(ll i=1;i<n;i++){
+        gv=__gcd(gv,a[i]);
+    }
+    
+    cout << gv << endl;
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
