d92dc7a9eb72d5a1a0b2780ac8571b82e9ea26ef
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-08-25 16:49:16 +0530

    Create Chef And Salary Pay.cpp

---
+++ Chef And Salary Pay.cpp
@@ -0,0 +1,119 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 25th August 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/HCAGMAM1?tab=ide
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    // ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    
+    int n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    
+    int cnt=0;
+    int z=0,mx=0;
+    for(int i=0;i<30;i++){
+        if(s[i]=='1')
+            cnt++;
+        else{
+            mx=max(mx,cnt);
+            cnt=0;
+            z++;
+        }
+    }
+    
+    
+    // cout << max(mx,cnt) << endl;
+    
+    cout << max(mx,cnt)*k + (30-z)*n << endl;
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
