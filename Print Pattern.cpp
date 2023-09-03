bec1c23669b374bd3acdd57605c56484b81d7b2c
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-09-03 16:11:46 +0530

    Create Print Pattern.cpp

---
+++ Print Pattern.cpp
@@ -0,0 +1,121 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date ->  September 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/PPATTERN
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    // ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    
+    int n;
+    cin>>n;
+    
+    int ans=1;
+    int c=0;
+    int a[100][100]={0};
+    while(ans!=n*n+1){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i+j == c){
+                    a[i][j]=ans;
+                    ans++;
+                }
+            }
+        }
+        c++;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            cout << a[i][j] <<" ";
+        
+        cout << endl;
+    }
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
