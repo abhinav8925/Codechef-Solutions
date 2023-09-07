cdf6f0057f36232c4b64f2a1649c76d9e803e0dc
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-09-07 18:54:17 +0530

    Create Closing the Tweets.cpp

---
+++ Closing the Tweets.cpp
@@ -0,0 +1,136 @@
+/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 7th September 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/TWTCLOSE
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    // ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    
+    
+    int n, q;
+    cin >> n >> q;
+    
+    int a[n+1]={0};
+    int cnt = 0;
+    
+    while (q--) {
+        string s;
+        cin >> s;
+        
+        if(s=="CLICK"){
+            int num;
+            cin>>num;
+            
+            if(a[num]==0){
+                cnt++;
+                a[num]=1;
+            }
+            else{
+                cnt--;
+                a[num]=0;
+            }
+        }
+        else{
+            cnt=0;
+            for(int i=1;i<=n;i++)
+                a[i]=0;
+            
+        }
+        
+        
+        
+        cout << cnt << endl;
+    }
+    
+
+    
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+// 	int t;
+// 	cin>>t;
+// 	while(t--)
+// 	{
+		solve();
+// 	}
+	return 0;
+}
