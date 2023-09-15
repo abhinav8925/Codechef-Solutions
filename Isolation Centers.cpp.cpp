f3a2f2a7f4df81dcba8d7d14696ec55f474de13d
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-09-15 08:36:42 +0530

    Create Isolation Centers.cpp.cpp

---
+++ Isolation Centers.cpp.cpp
@@ -0,0 +1,121 @@
+Isolation Centers.cpp/*
+  ***** !!!!! JAI SHREE KRISHNA !!!!! *****
+  
+  Date -> 15th September 2023
+  Code by - Abhinav Anand
+
+  Problem link -> https://www.codechef.com/problems/CORUS
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl "\n"
+using namespace std;
+int N=1000001;
+int mod=1000000007;
+vector<int> arr(N,true);
+vector<int> prime_num;
+vector<int> prime_fact(N,-1);
+
+void sieve()
+{
+    
+    arr[0]=arr[1]=0;
+    for(int i=2;i<=sqrt(N);i++)
+    {
+        if(arr[i]==1)
+        {
+            for(int j=i*i;j<=N;j+=i)
+                arr[j]=0;
+        }
+    }
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(arr[i])
+            prime_num.push_back(i);
+    }
+}
+
+void sieve_prime_factor()
+{
+    
+    for(int i=2;i<=N;i++)
+    {
+        if(prime_fact[i]==-1)
+        {
+            for(int j=i;j<=N;j+=i)
+            {
+                // if(prime_fact[j]==-1)
+                    prime_fact[j]=i;
+            }
+        }
+    }
+    
+    int k;
+    cin>>k;
+    while(k>1)
+    {
+        cout << prime_fact[k] << " ";
+        k=k/prime_fact[k];
+    }
+}
+
+bool isprime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+
+
+void solve()
+{
+    // ios_base::sync_with_stdio(0);
+    // cout.precision(7);
+    
+    int n,q;
+    cin>>n>>q;
+    
+    string s;
+    cin>>s;
+    
+    int f[26]={0};
+    
+    for(int i=0;i<n;i++)
+        f[s[i]-'a']++;
+    
+    while(q--){
+        int m;
+        cin>>m;
+        int cnt=0;
+        for(int j=0;j<26;j++){
+            if(f[j]>m){
+                cnt+=f[j]-m;
+            }
+        }
+        
+        cout << cnt << endl;
+    }
+    
+    
+}
+
+int main() {
+	
+	//sieve_prime_factor();
+	// sieve();
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
